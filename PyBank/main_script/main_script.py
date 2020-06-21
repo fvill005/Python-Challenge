b802ef5adbcb3a60c1247769204728e2fe3451c9
Filiberto Villegas <filibertovillegas92@gmail.com>
2020-06-20 17:49:33 -0700

    final update

--- PyBank/main_script/main_script.py
+++ PyBank/main_script/main_script.py
@@ -1,7 +1,6 @@
 import os
 import csv
 
-
 #declare variables and lists
 month_count = 0 
 total_revenue = 0 
@@ -10,14 +9,13 @@ profit_change = 0
 total_change = 0
 average_change = []
 total_profit = []
+monthly_profit_change = []
 greatest_inc = 0 
 max_val = 0
 min_val = 1000000000
 
-
+#read in the data
 budget_csv = os.path.join('..', 'Resources','budget_data.csv')
-#output_path = os.path.join('..', 'output, 'Analysis.txt')
-
 
 
 # Open and read csv
@@ -29,14 +27,15 @@ with open(budget_csv) as csvfile:
     
 
     # Read through each row of data after the header
+    #find the total months and revenue 
     for row in csvreader:
         month_count += 1
         total_revenue += int(row[1])
         total_profit.append(int(row[1]))
         average_change.append(int(row[1]))
-        avg_change = round(sum(average_change)/ month_count, 2)
+    
 
-        
+        #find the greatest and lowest months 
         if int(row[1]) > max_val:
             max_val = int(row[1])
             greatest_month = str(row[0])
@@ -44,21 +43,22 @@ with open(budget_csv) as csvfile:
         if int(row[1]) < min_val:
             min_val = int(row[1])
             lowest_month = str(row[0])
-
     
+    #find the average change month over month 
+    for i in range(len(total_profit)-1):
+        #ind the change and add it to the list 
+        monthly_profit_change.append(total_profit[i+1]-total_profit[i])
 
-      
-
-
+# print to terminal 
 print("Financial Analysis")
 print("----------------------------------------")
 print(f"Total Months: {month_count}")
 print(f"Total: ${total_revenue}")
-print(f"Average Change: ${avg_change}")
+print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
 print(f"Greatest Increase in Profits: {greatest_month} (${max_val})")
 print(f"Greatest Decrease in Profits: {lowest_month} (${min_val})")
 
-
+#create text file and 
 output_path = os.path.join('..', 'analysis', 'analysis.txt')
 #output to text file
 
@@ -67,9 +67,10 @@ with open(output_path,'w') as text:
     text.write("----------------------------------------" + "\n")
     text.write(f"Total Months: {month_count}" + "\n")
     text.write(f"Total Revenue: ${total_revenue}" + "\n")
-    text.write(f"Average Change: ${avg_change}" + "\n")
+    text.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}" + "\n")
     text.write(f"Greatest Increase in Profits: {greatest_month} (${max_val})" + "\n")
     text.write(f"Greatest Decrease in Profits: {lowest_month} (${min_val})" + "\n")
+
 
 
 

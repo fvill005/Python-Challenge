02075ac5cb544f4db26968b5ff5aa91674c98b5c
Filiberto Villegas <filibertovillegas92@gmail.com>
2020-06-20 17:36:45 -0700

    updated

--- PyBank/main/main.py
+++ PyBank/main/main.py
@@ -1,22 +1,18 @@
 import os
 import csv
 
+
+#declare variables and lists
 month_count = 0 
 total_revenue = 0 
 int_revenue = 0 
 profit_change = 0
 total_change = 0
 average_change = []
-running_tot = 0
-running_diff = 0 
-diff_tot =0 
-last_amt = 0
+total_profit = []
 greatest_inc = 0 
 max_val = 0
 min_val = 1000000000
-
-
-
 
 
 budget_csv = os.path.join('..', 'Resources','budget_data.csv')
@@ -36,15 +32,11 @@ with open(budget_csv) as csvfile:
     for row in csvreader:
         month_count += 1
         total_revenue += int(row[1])
+        total_profit.append(int(row[1]))
         average_change.append(int(row[1]))
         avg_change = round(sum(average_change)/ month_count, 2)
-        running_tot = running_tot + total_revenue
-        running_diff = total_revenue - last_amt
-        last_amt = total_revenue
-        diff_tot = diff_tot + running_diff
 
-        avg_diff = (round(diff_tot/ month_count - 1), 2)
-
+        
         if int(row[1]) > max_val:
             max_val = int(row[1])
             greatest_month = str(row[0])
@@ -62,7 +54,7 @@ print("Financial Analysis")
 print("----------------------------------------")
 print(f"Total Months: {month_count}")
 print(f"Total: ${total_revenue}")
-print(f"Average Change: ${avg_diff}")
+print(f"Average Change: ${avg_change}")
 print(f"Greatest Increase in Profits: {greatest_month} (${max_val})")
 print(f"Greatest Decrease in Profits: {lowest_month} (${min_val})")
 
@@ -75,8 +67,8 @@ with open(output_path,'w') as text:
     text.write("----------------------------------------" + "\n")
     text.write(f"Total Months: {month_count}" + "\n")
     text.write(f"Total Revenue: ${total_revenue}" + "\n")
-    #text.write(f"Average Change: ${avg_change}" + "\n")
-    text.write(f"Greatest Increase in Profits: {greatest_month} (${max_val}" + "\n")
+    text.write(f"Average Change: ${avg_change}" + "\n")
+    text.write(f"Greatest Increase in Profits: {greatest_month} (${max_val})" + "\n")
     text.write(f"Greatest Decrease in Profits: {lowest_month} (${min_val})" + "\n")
 
 
--- PyPoll/main_script/main.py
+++ PyPoll/main_script/main.py
@@ -1,11 +1,17 @@
 import os
 import csv
 
+#declare variables, list, dict
 total_votes = 0
 candidates = []
-candidates_votes = {}
+candidates_votes = []
+election_results = {}
+Khan_votes = 0
+Correy_votes = 0
+Li_votes = 0
+Otooley_votes = 0 
 
-
+#find file and read data
 election = os.path.join('..', 'Resources','election_data.csv')
 
 with open (election) as csvfile:
@@ -13,31 +19,72 @@ with open (election) as csvfile:
     print(csvreader)
 
     csv_header = next(csvreader)
-    #print (f"CSV Header: {csv_header}")
 
+    #tally the total votes
     for row in csvreader:
         total_votes += 1
         candidates_name= row[2]
 
+        #find the candidates
         if candidates_name not in candidates:
             candidates.append(candidates_name)
-            candidates_votes[candidates_name] = +1
-            #candidates_votes[candidates_name] = candidates_votes[candidates_name] + 1
+         #found 4 candidates who recieved votes, and add up each time their name appears    
+        if candidates_name == "Khan":
+            Khan_votes += 1
+        elif candidates_name == "Correy":
+            Correy_votes += 1
+        elif candidates_name == "Li":
+            Li_votes += 1
+        elif candidates_name == "O'Tooley":
+            Otooley_votes += 1
+    
+    #find percentage of votes
+    khan_percent = (Khan_votes/total_votes) *100
+    correy_percent = (Correy_votes/total_votes) *100
+    li_percent = (Li_votes/total_votes) *100
+    otooley_percent = (Otooley_votes/total_votes) *100
+
+    #find out who won
+    election_results ={"Khan": khan_percent, 
+                        "Correy": correy_percent, 
+                        "Li": li_percent, 
+                        "O'Tooley": otooley_percent}
+    #results
+    max_key = max(election_results, key = election_results.get)
+
+           
     
     
    
 
 
-
+#print to terminal 
 print("Election Results")
 print("----------------------------------------")
 print(f"Total Votes: {total_votes}")
 print("----------------------------------------")
-#print(candidates)
-print(candidates_votes)
-
+print(f"Khan: {khan_percent: .3f}% ({Khan_votes})")
+print(f"Correy: {correy_percent: .3f}% ({Correy_votes})")
+print(f"Li: {li_percent: .3f}% ({Li_votes})")
+print(f"O'Tooley: {otooley_percent: .3f}% ({Otooley_votes})")
 print("----------------------------------------")
-print("Winner:")
+print(f"Winner: {max_key}")
 print("----------------------------------------")
 
+#create text file 
+output_path = os.path.join('..', 'analysis', 'analysis.txt')
+#output to text file
+
+with open(output_path,'w') as text:
+    text.write("Election Results" + "\n")
+    text.write("----------------------------------------" + "\n")
+    text.write(f"Total Votes: {total_votes}" + "\n")
+    text.write("----------------------------------------" + "\n")
+    text.write(f"Khan: {khan_percent: .3f}% ({Khan_votes})"+ "\n")
+    text.write(f"Correy: {correy_percent: .3f}% ({Correy_votes})"+ "\n")
+    text.write(f"Li: {li_percent: .3f}% ({Li_votes})" + "\n")
+    text.write(f"O'Tooley: {otooley_percent: .3f}% ({Otooley_votes})" + "\n")
+    text.write("----------------------------------------" + "\n")
+    text.write(f"Winner: {max_key}" + "\n")
+    text.write("----------------------------------------" + "\n")
 
